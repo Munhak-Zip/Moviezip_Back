0eba5e9240df0dd565494d340494db6a03227d09
Dayun Lee <wohd7877@naver.com>
2024-05-10 11:27:50 +0900

    [add]

---
+++ demo/src/main/java/com/example/moviezip/MoviezipApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviezip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviezipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviezipApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/example/moviezip/MoviezipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviezip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviezipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
