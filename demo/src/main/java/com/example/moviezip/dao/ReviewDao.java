ab60790694d11fe98b278a673e26b8de124679dd
yjin-jo <dpwls5100@icloud.com>
2024-05-11 22:07:25 +0900

    [add] : getReviewList 추가 #2

--- demo/src/main/java/com/example/moviezip/dao/ReviewDao.java
+++ demo/src/main/java/com/example/moviezip/dao/ReviewDao.java
@@ -3,9 +3,13 @@ package com.example.moviezip.dao;
 import com.example.moviezip.domain.Review;
 import org.springframework.dao.DataAccessException;
 
+import java.util.List;
+
 public interface ReviewDao {
 
     void insertMyReview(Review review) throws DataAccessException;
 
     void deleteReview(long rvId) throws DataAccessException;
+
+    List<Review> getReviewList(long mvId) throws DataAccessException;
 }
