eb9d718c62bf7e123a5bf0aa1f21bb66606d81c4
yjin-jo <dpwls5100@icloud.com>
2024-05-11 22:07:39 +0900

    [add] : getReviewList 추가 #2

--- demo/src/main/java/com/example/moviezip/dao/mybatis/mapper/ReviewMapper.java
+++ demo/src/main/java/com/example/moviezip/dao/mybatis/mapper/ReviewMapper.java
@@ -3,8 +3,11 @@ package com.example.moviezip.dao.mybatis.mapper;
 import com.example.moviezip.domain.Review;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface ReviewMapper {
     void insertMyReview(Review review);
     void deleteReview(long rvId);
+    List<Review> getReviewList(long mvId);
 }
