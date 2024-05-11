0255ff36df5025243d4d471827dd894caae1bf64
yjin-jo <dpwls5100@icloud.com>
2024-05-11 20:40:39 +0900

    [add] : MybatisReviewDao.java 추가

--- demo/src/main/java/com/example/moviezip/dao/mybatis/MybatisReviewDao.java
+++ demo/src/main/java/com/example/moviezip/dao/mybatis/MybatisReviewDao.java
@@ -1,11 +1,18 @@
 package com.example.moviezip.dao.mybatis;
 
+import com.example.moviezip.dao.ReviewDao;
 import com.example.moviezip.dao.mybatis.mapper.ReviewMapper;
+import com.example.moviezip.domain.Review;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class MybatisReviewDao {
+public class MybatisReviewDao implements ReviewDao {
     @Autowired
     private ReviewMapper reviewMapper;
+    @Override
+    public void insertMyReview(Review review) throws DataAccessException {
+        reviewMapper.insertMyReview(review);
+    }
 }
