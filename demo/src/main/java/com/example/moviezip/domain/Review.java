995fd81dfcca0d9ea28572196baac6e4753a5f49
yjin-jo <dpwls5100@icloud.com>
2024-05-11 22:08:55 +0900

    [add] : Serializable 추가 #2

--- demo/src/main/java/com/example/moviezip/domain/Review.java
+++ demo/src/main/java/com/example/moviezip/domain/Review.java
@@ -1,6 +1,8 @@
 package com.example.moviezip.domain;
 
-public class Review {
+import java.io.Serializable;
+
+public class Review implements Serializable {
     private int mvId;
     private int rvId;
     private int rvStar;
