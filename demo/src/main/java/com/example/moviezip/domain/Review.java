dcda7ad5a4d7bfcbb67bea036cda66b20bfb5fba
yjin-jo <dpwls5100@icloud.com>
2024-05-11 19:50:48 +0900

    [add] : Review.java 생성자 추가

--- demo/src/main/java/com/example/moviezip/domain/Review.java
+++ demo/src/main/java/com/example/moviezip/domain/Review.java
@@ -3,6 +3,7 @@ package com.example.moviezip.domain;
 public class Review {
     private int mvId;
     private int rvId;
+    private int rvStar;
     private String mvTitle;
     private String wrtier;
     private String content;
@@ -10,5 +11,11 @@ public class Review {
 
     public Review(){
 
+    }
+
+    public Review(String content, int star, int mvId) { // id 지웠음, 예진이 수정 필요
+        this.content = content;
+        this.rvStar = star;
+        this.mvId = mvId;
     }
 }
