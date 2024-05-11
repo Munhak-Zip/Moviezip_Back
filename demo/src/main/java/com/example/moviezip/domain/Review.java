ed56f628634a6e572a833d317ac74d6f4da3122a
yjin-jo <dpwls5100@icloud.com>
2024-05-11 19:51:41 +0900

    [add] : Review.java getter,setter 추가

--- demo/src/main/java/com/example/moviezip/domain/Review.java
+++ demo/src/main/java/com/example/moviezip/domain/Review.java
@@ -18,4 +18,60 @@ public class Review {
         this.rvStar = star;
         this.mvId = mvId;
     }
+
+    public int getMvId() {
+        return mvId;
+    }
+
+    public void setMvId(int mvId) {
+        this.mvId = mvId;
+    }
+
+    public int getRvId() {
+        return rvId;
+    }
+
+    public void setRvId(int rvId) {
+        this.rvId = rvId;
+    }
+
+    public int getRvStar() {
+        return rvStar;
+    }
+
+    public void setRvStar(int rvStar) {
+        this.rvStar = rvStar;
+    }
+
+    public String getMvTitle() {
+        return mvTitle;
+    }
+
+    public void setMvTitle(String mvTitle) {
+        this.mvTitle = mvTitle;
+    }
+
+    public String getWrtier() {
+        return wrtier;
+    }
+
+    public void setWrtier(String wrtier) {
+        this.wrtier = wrtier;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getRvTitle() {
+        return rvTitle;
+    }
+
+    public void setRvTitle(String rvTitle) {
+        this.rvTitle = rvTitle;
+    }
 }
