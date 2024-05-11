d3c771235ebc410387e960304dc8ac48cbb26699
yjin-jo <dpwls5100@icloud.com>
2024-05-11 19:49:04 +0900

    [add] : Review.java 변수 추가

--- demo/src/main/java/com/example/moviezip/domain/Review.java
+++ demo/src/main/java/com/example/moviezip/domain/Review.java
@@ -1,4 +1,10 @@
 package com.example.moviezip.domain;
 
 public class Review {
+    private int mvId;
+    private int rvId;
+    private String mvTitle;
+    private String wrtier;
+    private String content;
+    private String rvTitle;
 }
