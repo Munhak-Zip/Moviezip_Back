8dc69f7834e09964e483460dff7a98709e182728
yjin-jo <dpwls5100@icloud.com>
2024-05-17 12:41:29 +0900

    [add] : insertReservation 완성 #17

--- demo/src/main/java/com/example/moviezip/dao/mybatis/MybatisReservationDao.java
+++ demo/src/main/java/com/example/moviezip/dao/mybatis/MybatisReservationDao.java
@@ -1,12 +1,20 @@
 package com.example.moviezip.dao.mybatis;
 
+import com.example.moviezip.dao.ReservationDao;
 import com.example.moviezip.dao.mybatis.mapper.ReservationMapper;
+import com.example.moviezip.domain.Reservation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class MybatisReservationDao {
+public class MybatisReservationDao implements ReservationDao {
     @Autowired
     private ReservationMapper reservationMapper;
+
+    @Override
+    public void insertReservation(Reservation reservation) throws DataAccessException {
+        reservationMapper.insertReservation(reservation);
+    }
 
 }
