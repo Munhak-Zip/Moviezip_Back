8ffc510c5e0a1cb293a53f68910b93ef89b27059
yjin-jo <dpwls5100@icloud.com>
2024-05-17 11:32:25 +0900

    [add] : ReservationMapper.java 추가 #17

---
+++ demo/src/main/java/com/example/moviezip/dao/mybatis/mapper/ReservationMapper.java
@@ -0,0 +1,10 @@
+package com.example.moviezip.dao.mybatis.mapper;
+
+import com.example.moviezip.domain.Review;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface ReservationMapper {
+}
