3680e46689d35cf4b32be420dfba02ad22c51ad8
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2024-05-17 12:17:13 +0900

    [add] : Review 도메인 is_Critic 추가 #16

--- demo/src/main/java/com/example/moviezip/domain/Review.java
+++ demo/src/main/java/com/example/moviezip/domain/Review.java
@@ -1,15 +1,20 @@
 package com.example.moviezip.domain;
 
+import lombok.Getter;
+
 import java.io.Serializable;
 
+@Getter
 public class Review implements Serializable {
     private int mvId;
     private int rvId;
     private int rvStar;
     private String mvTitle;
-    private String wrtier;
+    private String writer;
     private String content;
     private String rvTitle;
+    private String is_Critic;
+
 
     public Review(){
 
@@ -21,59 +26,36 @@ public class Review implements Serializable {
         this.mvId = mvId;
     }
 
-    public int getMvId() {
-        return mvId;
-    }
 
     public void setMvId(int mvId) {
         this.mvId = mvId;
-    }
-
-    public int getRvId() {
-        return rvId;
     }
 
     public void setRvId(int rvId) {
         this.rvId = rvId;
     }
 
-    public int getRvStar() {
-        return rvStar;
-    }
-
     public void setRvStar(int rvStar) {
         this.rvStar = rvStar;
-    }
-
-    public String getMvTitle() {
-        return mvTitle;
     }
 
     public void setMvTitle(String mvTitle) {
         this.mvTitle = mvTitle;
     }
 
-    public String getWrtier() {
-        return wrtier;
-    }
-
-    public void setWrtier(String wrtier) {
-        this.wrtier = wrtier;
-    }
-
-    public String getContent() {
-        return content;
+    public void setWriter(String writer) {
+        this.writer = writer;
     }
 
     public void setContent(String content) {
         this.content = content;
     }
 
-    public String getRvTitle() {
-        return rvTitle;
-    }
-
     public void setRvTitle(String rvTitle) {
         this.rvTitle = rvTitle;
+    }
+
+    public void setIs_Critic(String is_Critic) {
+        this.is_Critic = is_Critic;
     }
 }
