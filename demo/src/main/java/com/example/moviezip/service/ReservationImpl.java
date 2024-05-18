deed6996af965191a33e0254f2df83e6000cfd05
yjin-jo <dpwls5100@icloud.com>
2024-05-18 17:01:44 +0900

    [add] : getReservationById 추가 #17

--- demo/src/main/java/com/example/moviezip/service/ReservationImpl.java
+++ demo/src/main/java/com/example/moviezip/service/ReservationImpl.java
@@ -1,7 +1,10 @@
 package com.example.moviezip.service;
 
+import com.example.moviezip.dao.ReservationDao;
 import com.example.moviezip.dao.mybatis.MybatisReservationDao;
 import com.example.moviezip.domain.Reservation;
+
+import java.util.List;
 
 public class ReservationImpl {
     private final MybatisReservationDao mybatisReservationDao;
@@ -11,4 +14,6 @@ public class ReservationImpl {
     public void insertReservation(Reservation reservation) { mybatisReservationDao.insertReservation(reservation);}
     public int updateReservation(Reservation reservation) { return mybatisReservationDao.updateReservation(reservation);}
     public void deleteReservation(Long reserveId) { mybatisReservationDao.deleteReservation(reserveId);}
+    public List<Reservation> getReservationById(Long id) { return mybatisReservationDao.getReservationById(id);}
+
 }
