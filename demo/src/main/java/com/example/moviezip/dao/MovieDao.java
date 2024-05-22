d51e78658248cb610f18f780c629aa14c69422b5
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2024-05-22 21:22:59 +0900

    [add] : MovieDao interface 최신영화검색 getRecentMovie() 추가 #15

--- demo/src/main/java/com/example/moviezip/dao/MovieDao.java
+++ demo/src/main/java/com/example/moviezip/dao/MovieDao.java
@@ -1,11 +1,16 @@
 package com.example.moviezip.dao;
 
 import com.example.moviezip.domain.Movie;
+import org.springframework.dao.DataAccessException;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface MovieDao {
     //영화 검색
     public List<Movie> searchMovieByKeyword(String keyword);
+
+    //최신 영화 검색
+    public List<Movie> getRecentMovie();
 
 }
