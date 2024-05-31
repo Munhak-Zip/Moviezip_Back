8be9db31de9a6667f6fe306f3d4257202ff024ae
Dayun Lee <wohd7877@naver.com>
2024-05-31 13:00:28 +0900

    [add/#25] 사용자 맞춤형 알고리즘 영화

--- demo/src/main/java/com/example/moviezip/controller/MovieRecommendController.java
+++ demo/src/main/java/com/example/moviezip/controller/MovieRecommendController.java
@@ -1,16 +1,26 @@
 package com.example.moviezip.controller;
 
+import com.example.moviezip.service.recommend.MovieRecommenderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
 public class MovieRecommendController {
-
-    @GetMapping("/api/hello")
-    public String test() {
-        return "Hello, world!";
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/main/recommend")
+    public List<String> recommendMovies(@RequestParam int userId) {
+        // MovieRecommender 실행
+        List<String> recommendations = MovieRecommenderService.recommendMovies(userId); // 수정된 부분
+        if (recommendations.isEmpty()) {
+            System.out.println("없다");
+        }
+        for (String str : recommendations) {
+            System.out.println("값" + str);
+        }
+        return recommendations;
     }
 }
