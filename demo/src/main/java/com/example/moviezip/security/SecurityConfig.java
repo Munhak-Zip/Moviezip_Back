2b00e4a298a2dbcceb494b87721fdbc7d6e1507e
Dayun Lee <wohd7877@naver.com>
2024-06-04 16:47:46 +0900

    [fix] : pom.xml 수정 #27

--- demo/src/main/java/com/example/moviezip/security/SecurityConfig.java
+++ demo/src/main/java/com/example/moviezip/security/SecurityConfig.java
@@ -1,60 +1,75 @@
 package com.example.moviezip.security;
-
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
-    //Security Config 로그인 페이지 설정 및 로그인 경로
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean //암호화
+    @Bean // 암호화
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .cors(withDefaults()) // CORS 설정 활성화
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/",  "/loginProc", "/join","joinProc").permitAll()
-                        .anyRequest().authenticated()
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .antMatchers("/", "/loginProc", "/join", "/joinProc").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(formLogin ->
+                        formLogin
+                                .loginPage("/login")
+                                .loginProcessingUrl("/loginProc")
+                                .usernameParameter("username") // 폼 필드 이름과 일치시킵니다.
+                                .successHandler(successHandler())
+                                .failureHandler(failureHandler())
+                                .permitAll()
+                )
+                .csrf(csrf ->
+                        csrf.disable()
                 );
-
-
-        http
-                .formLogin((auth) -> auth.loginPage("/login")
-                        .loginProcessingUrl("/loginProc")
-                        .usernameParameter("username") // 폼 필드 이름과 일치시킵니다.
-                        .successHandler((request, response, authentication) -> {
-                            response.setStatus(HttpServletResponse.SC_OK);
-                        })
-                        .failureHandler(new SimpleUrlAuthenticationFailureHandler())
-                        .permitAll()
-                );
-
-        http
-                .csrf((auth) -> auth.disable());
-
-
-
-
-        return http.build();
     }
+
+    @Bean
+    public AuthenticationSuccessHandler successHandler() {
+        return new SimpleUrlAuthenticationSuccessHandler() {
+            @Override
+            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, org.springframework.security.core.Authentication authentication) throws IOException, ServletException {
+                response.setStatus(HttpServletResponse.SC_OK);
+            }
+        };
+    }
+
+    @Bean
+    public AuthenticationFailureHandler failureHandler() {
+        return new SimpleUrlAuthenticationFailureHandler();
+    }
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
