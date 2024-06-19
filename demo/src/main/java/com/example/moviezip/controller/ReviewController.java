8f381e2eac7b25fa93ae90d38d2f2639224c502b
Dayun Lee <wohd7877@naver.com>
2024-06-19 21:35:39 +0900

    [feat] 평론가 리뷰 제공 #48

--- demo/src/main/java/com/example/moviezip/controller/ReviewController.java
+++ demo/src/main/java/com/example/moviezip/controller/ReviewController.java
@@ -5,6 +5,8 @@ import com.example.moviezip.service.ReviewImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/movie")
 public class ReviewController {
@@ -30,5 +32,19 @@ public class ReviewController {
             e.printStackTrace();
             // 필요에 따라 예외 처리 로직 추가
         }
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("{mvId}/critics")
+    public List<Review> getCriticsReview(@PathVariable int mvId) throws Exception {
+        System.out.println("Entering getMovie method with mvId: " + mvId);
+        List<Review> rv = reviewImpl.getCriticReviews(mvId);
+
+        for (Review r : rv) {
+            System.out.println("내용" + r.getContent());
+            System.out.println("이름" + r.getRvTitle());
+            System.out.println("별점" + r.getRvStar());
+        }
+        return rv;
     }
 }
