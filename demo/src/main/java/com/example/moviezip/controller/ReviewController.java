358e078c91306cf91d997278f1355b1a24b87df0
yjin-jo <dpwls5100@icloud.com>
2024-06-19 19:03:55 +0900

    [feat] : insertMyReview 추가 #46

--- demo/src/main/java/com/example/moviezip/controller/ReviewController.java
+++ demo/src/main/java/com/example/moviezip/controller/ReviewController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/movie/mv")
+@RequestMapping("/movie")
 public class ReviewController {
 
     private final ReviewImpl reviewImpl;
@@ -15,13 +15,18 @@ public class ReviewController {
     public ReviewController(ReviewImpl reviewImpl) {
         this.reviewImpl = reviewImpl;
     }
+
     @CrossOrigin(origins = "http://localhost:3000")
-    @PostMapping("/add")
-    public void insertMyReview(@RequestBody Review review) {
+    @PostMapping("/{mvId}/regReview")
+    public void insertMyReview(@RequestBody Review review, @PathVariable Long mvId) {
         try {
-            reviewImpl.insertMyReview(review);
-            System.out.print("리뷰내용"+review.getContent());
+            // 필수 필드 설정
+            review.setMvId(mvId); // Review 객체에 mvId 설정
+            review.setIs_Critic("N"); // 평론가 여부 설정
+            reviewImpl.insertMyReview(review); // 서비스로 리뷰 저장 요청
+            System.out.println("리뷰가 성공적으로 저장되었습니다: " + review.getContent());
         } catch (Exception e) {
+            System.out.println("실패");
             e.printStackTrace();
             // 필요에 따라 예외 처리 로직 추가
         }
