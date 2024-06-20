e7cfe8856d221036b92fa5f0a4de58cb294e6a58
yjin-jo <dpwls5100@icloud.com>
2024-06-20 15:37:41 +0900

    [feat] : rvID타입 수정 #46

--- demo/src/main/java/com/example/moviezip/dao/mybatis/MybatisReviewDao.java
+++ demo/src/main/java/com/example/moviezip/dao/mybatis/MybatisReviewDao.java
@@ -32,7 +32,7 @@ public class MybatisReviewDao implements ReviewDao {
         return reviewMapper.getReviewList(mvId);
     }
     @Override
-    public Review getReviewDetail(long rvId) throws DataAccessException {
+    public Review getReviewDetail(int rvId) throws DataAccessException {
         return reviewMapper.getReviewDetail(rvId);
     }
     @Override
