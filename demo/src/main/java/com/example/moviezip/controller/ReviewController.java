08b0d164489013b977dbc136b42c8b769d74b225
yjin-jo <dpwls5100@icloud.com>
2024-06-20 13:19:28 +0900

    [feat] : getMyReviewList 추가 #46

--- demo/src/main/java/com/example/moviezip/controller/ReviewController.java
+++ demo/src/main/java/com/example/moviezip/controller/ReviewController.java
@@ -1,34 +1,58 @@
 package com.example.moviezip.controller;
 
 import com.example.moviezip.domain.Review;
+import com.example.moviezip.domain.User;
 import com.example.moviezip.service.ReviewImpl;
+import com.example.moviezip.service.UserService;
+import com.example.moviezip.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/movie")
+@RequestMapping("/")
 public class ReviewController {
 
     private final ReviewImpl reviewImpl;
+    private final UserService userService;
 
     @Autowired
-    public ReviewController(ReviewImpl reviewImpl) {
+    public ReviewController(ReviewImpl reviewImpl, UserService userService) {
         this.reviewImpl = reviewImpl;
+        this.userService = userService;
+
     }
 
     @CrossOrigin(origins = "http://localhost:3000")
-    @PostMapping("/{mvId}/regReview")
+    @PostMapping("movie/{mvId}/regReview")
     public void insertMyReview(@RequestBody Review review, @PathVariable Long mvId) {
         try {
-            // 필수 필드 설정
-            review.setMvId(mvId); // Review 객체에 mvId 설정
-            review.setIs_Critic("N"); // 평론가 여부 설정
-            reviewImpl.insertMyReview(review); // 서비스로 리뷰 저장 요청
+            review.setMvId(mvId);
+            review.setIs_Critic("N");
+            reviewImpl.insertMyReview(review);
             System.out.println("리뷰가 성공적으로 저장되었습니다: " + review.getContent());
         } catch (Exception e) {
             System.out.println("실패");
             e.printStackTrace();
-            // 필요에 따라 예외 처리 로직 추가
         }
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("wish/myReviewList")
+    public List<Review> getMyReviewList(@RequestParam String userId) {
+        User user = userService.getUserById(userId);
+        String wrtier = user.getUserId();
+
+        List<Review> myReviewList = reviewImpl.getMyReviewList(wrtier);
+        try {
+            for (Review rv : myReviewList) {
+                System.out.println("내가 쓴 리뷰 :" + rv.getMvTitle()+rv.getContent());
+            }
+        } catch (Exception e) {
+            System.out.println("리뷰 실패:");
+            e.printStackTrace();
+        }
+        return myReviewList;
     }
 }
